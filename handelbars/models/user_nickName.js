3c8ac710b44d50693d568f5b4ecce57139449683
hgq1211 <717042959@qq.com>
2016-09-02 17:20:07 +0800

    ssss

--- handelbars/models/user_nickName.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Administrator on 2016/2/6 0006.
- */
-req.session.user.user_nickName;
--- handelbars/public/js/user_status.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Administrator on 2016/4/10.
- */
-function user_status(){
-
-}
